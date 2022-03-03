8cc1c43e9e9b03c9117282982f69ebeb2a6dabe0
yhnoh <tedgom19@gamil.com>
2022-03-03 17:20:44 +0900

    release

---
+++ src/main/java/com/example/springbootrelease/SpringBootReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReleaseApplication.class, args);
+    }
+
+}
